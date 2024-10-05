4f951028b322b2c1c28d74a2815173b899519254
Yuri Masalov <masalov.09@bk.ru>
2024-10-05 17:26:57 +0600

    AstroNvim settings. Date: 05.10.2024

---
+++ lua/plugins/astroui.lua
@@ -0,0 +1,9 @@
+-- if true then return {} end
+
+return {
+  "AstroNvim/astroui",
+  ---@type AstroUIOpts
+  opts = {
+    colorscheme = "catppuccin-frappe",
+  },
+}
---
+++ lua/plugins/sonokai.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "sainnhe/sonokai",
+    init = function() -- init function runs before the plugin is loaded
+      vim.g.sonokai_style = "shusia"
+    end,
+  },
+}
