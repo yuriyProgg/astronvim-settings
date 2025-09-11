fffa896a866515e9ecabd71a7271870bfdffaabd
Yuri Masalov <masalov.09@bk.ru>
2025-09-11 17:21:54 +0600

    Update config 11.09.2025

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -29,8 +29,7 @@ return {
       opt = {                      -- vim.opt.<key>
         relativenumber = true,     -- sets vim.opt.relativenumber
         number = true,             -- sets vim.opt.number
-        spell = true,              -- sets vim.opt.spell
-        spelllang = { "en", "ru" },
+        spell = false,              -- sets vim.opt.spell
         signcolumn = "yes",        -- sets vim.opt.signcolumn to yes
         wrap = true,               -- sets vim.opt.wrap
         clipboard = "unnamedplus", -- sets vim.opt.clipboard
